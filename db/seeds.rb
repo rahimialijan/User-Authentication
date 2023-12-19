c24e929c74f91fcaf3e2ec99fbd616f8c0b79e32
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-12-19 16:22:24 +0500

    config the devise for user authenticatoin and generat books controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.first_or_create(email: 'ali@test.com',
+    password: 'test',
+    password_confirmation: 'test',
+    role: User.roles[:admin])
